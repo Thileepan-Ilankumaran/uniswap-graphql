bc74e1122a69330b2d837d08911b378698821008
mohit-rakhade <mohitrakhade20@gmail.com>
2023-06-12 22:11:18 +0530

    use query and frontend added

--- src/components/App.js
+++ src/components/App.js
@@ -43,10 +43,57 @@ const ETH_PRICE_QUERY = gql`
 
 function App() {
 
+  const { loading: ethLoading, data: ethPriceData} = useQuery(ETH_PRICE_QUERY)
+  const { loading: daiLoading, data: daiData} = useQuery(DAI_QUERY, {
+    variables: {
+      tokenAddress: "0x6b175474e89094c44da98b954eedeac495271d0f"
+    }
+  })
+
+  const daiPriceInETH = daiData && daiData.tokens[0].derivedETH
+  const daiTotalLiquidity = daiData && daiData.tokens[0].totalLiquidity
+  const ethPriceInUSD = ethPriceData && ethPriceData.bundles[0].ethPrice
+
 
   return (
     <div>
-      hey lusterien
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          href="http://www.dappuniversity.com/bootcamp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={uniswapLogo} width="30" height="30" className="d-inline-block align-top" alt="" />
+          &nbsp; Uniswap Explorer
+        </a>
+      </nav>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex text-center">
+            <div className="content mr-auto ml-auto">
+              <div>
+                <img src={daiLogo} width="150" height="150" className="mb-4" alt="" />
+                <h2>
+                  Dai price:{' '}
+                  {ethLoading || daiLoading
+                    ? 'Loading token data...'
+                    : '$' +
+                      // parse responses as floats and fix to 2 decimals
+                      (parseFloat(daiPriceInEth) * parseFloat(ethPriceInUSD)).toFixed(2)}
+                </h2>
+                <h2>
+                  Dai total liquidity:{' '}
+                  {daiLoading
+                    ? 'Loading token data...'
+                    : // display the total amount of DAI spread across all pools
+                      parseFloat(daiTotalLiquidity).toFixed(0)}
+                </h2>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
